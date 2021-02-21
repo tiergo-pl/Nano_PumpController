445f84168d36e840557ff627c12fbb505ec0cb2b
Tiergo <tiergo@tiergo.pl>
2021-02-21 19:15:30 +0100

    stepHalfUp change

--- src/Stepper.cpp
+++ src/Stepper.cpp
@@ -35,40 +35,50 @@ void stepUp(){
 
 void stepHalfUp(){
   uint8_t tempPORTC = PORTC & 0b00001111;
-  stepperClr();
   switch (tempPORTC){
     case _BV(PC0):
+      PORTC &= ~(_BV(PC0));
       PORTC |= (_BV(PC0) | _BV(PC1));
     break;
 
     case (_BV(PC0) | _BV(PC1)):
+      PORTC &= ~(_BV(PC0) | _BV(PC1));
       PORTC |= _BV(PC1);
     break;
 
     case _BV(PC1):
+      PORTC &= ~(_BV(PC1));
       PORTC |= (_BV(PC1) | _BV(PC2));
     break;
 
     case (_BV(PC1) | _BV(PC2)):
+      PORTC &= ~(_BV(PC1) | _BV(PC2));
       PORTC |= _BV(PC2);
     break;
 
     case _BV(PC2):
+      PORTC &= ~(_BV(PC2));
       PORTC |= (_BV(PC2) | _BV(PC3));
     break;
 
     case (_BV(PC2) | _BV(PC3)):
+      PORTC &= ~(_BV(PC2) | _BV(PC3));
       PORTC |= _BV(PC3);
     break;
 
     case _BV(PC3):
+      PORTC &= ~(_BV(PC3));
       PORTC |= (_BV(PC3) | _BV(PC0));
     break;
 
-
-    default:
+    case (_BV(PC3) | _BV(PC0)):
+      PORTC &= ~(_BV(PC3) | _BV(PC0));
       PORTC |= _BV(PC0);
     break;
+
+    default:
+      stepperClr();
+      PORTC |= _BV(PC0);
 
   }
 }
